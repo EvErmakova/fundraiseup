ac7bed406ff62c42dae469e34e09d4bd9c8b57cf
Евгения Ермакова <russkih.evgeniya92@gmail.com>
2023-04-18 16:21:08 +0400

    init

---
+++ src/components/app/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <div>
+                Search
+            </div>
+            <div>
+                Menu
+            </div>
+            <div>
+                Info
+            </div>
+            <div>
+                Form
+            </div>
+        </div>
+    );
+}
+
+export default App;
